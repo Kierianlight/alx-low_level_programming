68337695d24c50c1ad6f243578f6316ca44cde38
Kierianlight <kierianchinedu@yahoo.com>
2023-02-16 19:11:20 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(voids)
+{
+	printf("\"with proper grammar, but the outcome is a pieceof art");
+	return (0);
+}	
